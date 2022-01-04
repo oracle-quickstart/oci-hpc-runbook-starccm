603bc24f1d109a93c34a4d3e11f5d1e622c3c4e6
Gloria Lee <glorialee820@gmail.com>
2022-01-04 10:42:59 -0500

    updated provider.tf with appropriate variables

--- provider.tf
+++ provider.tf
@@ -1,20 +1,20 @@
-## Copyright © 2021, Oracle and/or its affiliates. 
+## Copyright © 2022, Oracle and/or its affiliates. 
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   region           = var.region
-  api_fingerprint      = var.api_fingerprint
-  api_user_ocid        = var.api_user_ocid
-  api_user_key = var.api_user_key
+  fingerprint      = var.fingerprint
+  user_ocid        = var.user_ocid
+  private_key_path = var.private_key_path
 }
 
 provider "oci" {
   alias                = "homeregion"
   tenancy_ocid         = var.tenancy_ocid
-  api_user_ocid          = var.api_user_ocid
-  api_fingerprint          = var.api_fingerprint
-  api_user_key     = var.api_user_key
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
   region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
   disable_auto_retries = "true"
 }
